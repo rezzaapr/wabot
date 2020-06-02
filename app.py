49a5da109bc54f1237dd13a3566d4cbb39670998
Rezza Priatna <58212770+rezzaapr@users.noreply.github.com>
2020-06-02 22:50:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,18 @@ import json
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route("/")
+def hello():
+    return "Status Online"
+
+
+@app.route('/home', methods=['POST'])
 def home():
     if request.method == 'POST':
         bot = WABot(request.json)
         return bot.processing()
 
 if(__name__) == '__main__':
-    app.run()
+    app.run(host="localhost", port=8080, debug=True)
 
 
